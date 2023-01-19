875e6ee05ea396aadcab9463c48b7ea375738de4
Konrad Kapica <konradkapica@gmail.com>
2023-01-19 02:19:01 +0100

    another Tile added with skillsToLearn

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Container } from "./common/Container/styled";
 import Header from "./common/Header";
 import Tile from "./common/Tile";
+import { skills } from "./common/Tile/skills"
+import { skillsToLearn } from "./common/Tile/skillsToLearn"
 
 function App() {
   return (
     <Container>
       <Header />
-      <Tile 
-      title="My skillset includes 🛠️"
+      <Tile
+        title="My skillset includes 🛠️"
+        skills={skills}
       />
-
+      <Tile
+        title="What I want to learn next 🚀"
+        skills={skillsToLearn}
+      />
     </Container>
   );
 }
---
+++ src/common/Tile/skillsToLearn.js
@@ -0,0 +1,8 @@
+export const skillsToLearn = [
+    "React Context",
+    "Node.js",
+    "Cypress",
+    "Unit testing",
+    "TypeScript",
+    "JS classes",
+];
