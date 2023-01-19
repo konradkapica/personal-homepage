3ce00da10d490d35bb1f203fc5e2323fcc0c1f8e
Konrad Kapica <konradkapica@gmail.com>
2023-01-19 02:10:25 +0100

    list items from array in Tile component

--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -1,31 +1,14 @@
 import { StyledTile, Title, StyledList, Item } from "./styled";
 
-const Tile = ({ title }) => (
+const Tile = ({ title, skills }) => (
     <StyledTile>
         <Title>{title}</Title>
         <StyledList>
-            <Item>Semantic & accessible HTML</Item>
-            <Item>Responsive Web Design</Item>
-            <Item>Teamwork</Item>
-            <Item>Markdown</Item>
-            <Item>Immutability</Item>
-            <Item>CSS BEM convention</Item>
-            <Item>CSS Grid</Item>
-            <Item>CSS Flexbox</Item>
-            <Item>React Router</Item>
-            <Item>Redux-Saga</Item>
-            <Item>Redux (Toolkit)</Item>
-            <Item>React Hooks</Item>
-            <Item>Error handling</Item>
-            <Item>Working with API (fetch, axios)</Item>
-            <Item>JavaScript: ES6+</Item>
-            <Item>Promises, Async/Await</Item>
-            <Item>GitHub Pull Requests & Review</Item>
-            <Item>Trello</Item>
-            <Item>Scrum</Item>
-            <Item>NPM</Item>
-            <Item>React</Item>
-            <Item>Git</Item>
+            {skills.map((skill) => 
+                <Item key={skill}
+                >{skill}
+                </Item>
+            )}
         </StyledList>
     </StyledTile>
 );
