bb6eb4174686f2b03e9d8d7e3e24d7899e6dbb27
Konrad Kapica <konradkapica@gmail.com>
2023-01-19 01:56:26 +0100

    added new colors to theme

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,24 @@
 const colorNames = {
+    white: "#FFFFFF",
     mercury: "#e5e5e5",
+    slateGray: "#6E7E91",
+    scienceBlue: "#0366D6",
+    mineShaft: "#252525",
 }
 
 export const theme = {
+    button: {
+        background: colorNames.scienceBlue,
+        text: colorNames.white
+    },
+
     site: {
         background: colorNames.mercury,
-    }
+        text: colorNames.slateGray,
+        header: colorNames.mineShaft,
+    },
+
+    tile: {
+        background: colorNames.white
+    },
 }
