ebbd34298778e9efcf3668e85fa85f3f7eb51bfe
Konrad Kapica <konradkapica@gmail.com>
2023-01-19 15:34:31 +0100

    styles and icons added to Footer

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -1,10 +1,34 @@
+import {ReactComponent as Github} from "./icons/github.svg"
+import {ReactComponent as Facebook} from "./icons/facebook.svg"
+import {ReactComponent as Instagram} from "./icons/instagram.svg"
+import {ReactComponent as Linkedin} from "./icons/linkedin.svg"
+import { Wrapper, SubHeader, Header, Info, Container, Link } from "./styled";
+
 const Footer = () => (
     <>
-        <h2>Let’s talk!</h2>
-        <header>konradkapica@gmail.com</header>
-        <p>I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me
-        </p>
+        <Wrapper>
+            <SubHeader >Let's talk!</SubHeader >
+            <Header>konradkapica@gmail.com</Header>
+            <Info>
+                I'm always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me 🤞
+            </Info>
+        </Wrapper>
+        <Container>
+            <Link target="_blank" href="https://github.com/Rudolfini">
+                <Github />
+            </Link>
+            <Link target="_blank" href="https://www.linkedin.com/in/konrad-kapica-728529242/">
+                <Linkedin />
+            </Link>
+            <Link target="_blank" href="https://www.facebook.com/konrad.kapica.3">
+                <Facebook />
+            </Link>
+            <Link target="_blank" href="https://www.instagram.com/konradkapica/">
+                <Instagram />
+            </Link>
+        </Container>
     </>
+
 );
 
 export default Footer;
---
+++ src/common/Footer/styled.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    max-width: 670px;
+`;
+
+export const SubHeader = styled.h2`
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 1.3;
+    text-transform: uppercase;
+    margin: 0;
+    color: ${({theme}) => theme.site.text};
+`;
+
+export const Header = styled.header`
+    font-weight: 900;
+    font-size: 32px;
+    line-height: 39px;
+    letter-spacing: 0.05em;
+    color: ${({theme}) => theme.site.header};
+`;
+
+export const Info = styled.p`
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 1.4;
+    letter-spacing: 0.05em;
+    margin: 0;
+    color: ${({theme}) => theme.site.header};
+`;
+
+export const Container = styled.div`
+    margin-top: 56px;
+`;
+
+export const Link = styled.a`
+    display: inline-block;
+    width: 48px;
+    height: 48px;
+`;
