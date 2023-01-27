fbd9c82bad2f484afac2bca3b575a02d771e3f50
Konrad Kapica <konradkapica@gmail.com>
2023-01-27 23:39:14 +0100

    App.js simple

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import { Container } from "./features/PersonalHomepage/Container/styled";
-import Header from "./features/PersonalHomepage/Header";
-import Tile from "./features/PersonalHomepage/Tile";
-import Footer from "./features/PersonalHomepage/Footer";
-import Portfolio from "./features/PersonalHomepage/Portfolio";
-import { skills } from "./features/PersonalHomepage/Tile/skills"
-import { skillsToLearn } from "./features/PersonalHomepage/Tile/skillsToLearn"
+import PersonalHomepage from "./features/PersonalHomepage/PersonalHomepage";
 
 function App() {
-  return (
-    <Container>
-      <Header />
-      <Tile
-        title="My skillset includes 🛠️"
-        skills={skills}
-      />
-      <Tile
-        title="What I want to learn next 🚀"
-        skills={skillsToLearn}
-      />
-      <Portfolio />
-      <Footer />
-    </Container>
-  );
+    return (
+        <PersonalHomepage />
+    )
 }
 
 export default App;
