d7b92a7e25a0ddb49695644b479b62365ff84833
Konrad Kapica <konradkapica@gmail.com>
2023-01-30 21:16:25 +0100

    ButtonLink added to ReposError

--- src/features/PersonalHomepage/Portfolio/Repositories/ReposError/index.js
+++ src/features/PersonalHomepage/Portfolio/Repositories/ReposError/index.js
@@ -1,5 +1,6 @@
 import { WrapperError, IconError, HeaderError, InfoError } from "./styled";
 import {ReactComponent as Danger} from "./iconError/Danger.svg"
+import { ButtonLink } from "../../../ButtonLink";
 
 export const ReposError = () => (
     <WrapperError>
@@ -8,5 +9,6 @@ export const ReposError = () => (
         </IconError>
         <HeaderError>Ooops! Something went wrong... </HeaderError>
         <InfoError>Sorry, failed to load Github projects.<br/> You can check them directly on Github.</InfoError>
+        <ButtonLink target="_blank" href={"https://github.com/Rudolfini"}>Go to Github</ButtonLink>
     </WrapperError>
 );
