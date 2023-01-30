240eff18c5376965964994524504449fa5841292
Konrad Kapica <konradkapica@gmail.com>
2023-01-30 21:22:30 +0100

    MailButton moved out and renamed to ButtonLink

--- src/features/PersonalHomepage/Header/styled.js
+++ src/features/PersonalHomepage/Header/styled.js
@@ -46,27 +46,6 @@ export const Info = styled.p`
     color: ${({ theme }) => theme.site.text};
 `;
 
-export const MailButton = styled.a`
-    padding: 12px 16px;
-    gap: 16px;
-    position: relative;
-    width: 154px;
-    height: 49px;
-    border: 1px solid rgba(209, 213, 218, 0.3);
-    border-radius: 4px;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    text-decoration: none;
-    background-color: ${({ theme }) => theme.button.background};
-    
-    font-weight: 600;
-    font-size: 20.0584px;
-    line-height: 24px;
-    letter-spacing: 0.05em;
-    color: ${({ theme }) => theme.button.text};
-`;
-
 export const Message = styled(MessageItem)`
     width: 19.23px;
     height: 17.12px;
