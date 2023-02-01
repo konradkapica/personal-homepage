009e1a8e1d0d347a4c3f2991940047cab5881577
Konrad Kapica <konradkapica@gmail.com>
2023-02-01 12:02:16 +0100

    saga connected to store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import { personalHomepageSaga } from "./features/PersonalHomepage/personalHomepageSaga";
 import personalHomepageReducer from "./features/PersonalHomepage/personalHomepageSlice";
 
-export default configureStore({
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
     reducer: {
         personalHomepage: personalHomepageReducer,
-    }
+    },
+    middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(personalHomepageSaga);
+
+export default store;
