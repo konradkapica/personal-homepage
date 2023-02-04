48a951a2ad6200b78aa74bba81b8b7bea85ac858
Konrad Kapica <konradkapica@gmail.com>
2023-02-04 01:42:21 +0100

    Footer responsive, media query added,

--- src/features/PersonalHomepage/Footer/styled.js
+++ src/features/PersonalHomepage/Footer/styled.js
@@ -6,6 +6,11 @@ export const Wrapper = styled.div`
     gap: 24px;
     max-width: 670px;
     margin-top: 120px;
+
+    @media (max-width: 767px) {
+        gap: 12px;
+        margin-top: 48px;
+    }
 `;
 
 export const SubHeader = styled.h2`
@@ -19,15 +24,20 @@ export const SubHeader = styled.h2`
 
 export const Header = styled.header`
     font-weight: 900;
-    font-size: 32px;
+    font-size: clamp(18px, 5vw, 32px);
     line-height: 39px;
     letter-spacing: 0.05em;
     color: ${({theme}) => theme.site.header};
+    cursor: pointer;
+
+    &:hover {
+        color: ${({theme}) => theme.button.background}
+    }
 `;
 
 export const Info = styled.p`
     font-weight: 400;
-    font-size: 18px;
+    font-size: clamp(14px, 5vw, 18px);
     line-height: 1.4;
     letter-spacing: 0.05em;
     margin: 0;
@@ -36,6 +46,10 @@ export const Info = styled.p`
 
 export const Container = styled.div`
     margin-top: 56px;
+
+    @media (max-width: 767px) {
+        margin-top: 40px;
+    }
 `;
 
 export const Link = styled.a`
@@ -43,4 +57,15 @@ export const Link = styled.a`
     width: 48px;
     height: 48px;
     margin-right: 24px;
+    cursor: pointer;
+    filter: saturate(0) contrast(300%);
+
+    &:hover {
+        filter: saturate(100%);
+    }
+
+    @media (max-width: 767px) {
+        width: 31px;
+        height: 31px;
+    }
 `;
