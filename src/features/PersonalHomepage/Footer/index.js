324875dad92e81062fefc933999df614aff184d8
Konrad Kapica <konradkapica@gmail.com>
2023-02-04 01:41:39 +0100

    icons changed in Footer, email imported

--- src/features/PersonalHomepage/Footer/index.js
+++ src/features/PersonalHomepage/Footer/index.js
@@ -1,14 +1,15 @@
-import { ReactComponent as Github } from "./icons/github.svg"
-import { ReactComponent as Facebook } from "./icons/facebook.svg"
-import { ReactComponent as Instagram } from "./icons/instagram.svg"
-import { ReactComponent as Linkedin } from "./icons/linkedin.svg"
+import { ReactComponent as Github } from "./icons/github_LM.svg"
+import { ReactComponent as Facebook } from "./icons/facebook_LM.svg"
+import { ReactComponent as Instagram } from "./icons/instagram_LM.svg"
+import { ReactComponent as Linkedin } from "./icons/linkedin_LM.svg"
 import { Wrapper, SubHeader, Header, Info, Container, Link } from "./styled";
+import { email } from "../email";
 
 const Footer = () => (
     <>
         <Wrapper>
             <SubHeader >Let's talk!</SubHeader >
-            <Header>konradkapica@gmail.com</Header>
+            <Header>{email}</Header>
             <Info>
                 I'm always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me 🤞
             </Info>
