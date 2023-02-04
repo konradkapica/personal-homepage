6e90e6c3beaccd9b3cbbc3ac04ba5deb0751741b
Konrad Kapica <konradkapica@gmail.com>
2023-02-04 01:40:50 +0100

    media query added to Container

--- src/features/PersonalHomepage/Container/styled.js
+++ src/features/PersonalHomepage/Container/styled.js
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const Container = styled.main`
 max-width: 1216px;
 margin: auto;
+
+@media (max-width: 1289px) {
+        display: flex;
+        flex-direction: column;
+    }
 `;
