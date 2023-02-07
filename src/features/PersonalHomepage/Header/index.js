923542f931b15d4b7ba5bf6967be3d171c1bccf8
Konrad Kapica <konradkapica@gmail.com>
2023-02-07 20:55:48 +0100

    code formatted

--- src/features/PersonalHomepage/Header/index.js
+++ src/features/PersonalHomepage/Header/index.js
@@ -1,14 +1,14 @@
-import { 
-    Wrapper, 
-    Image, 
-    Content, 
-    SubHeader, 
-    Name, 
-    Info, 
-    Message 
+import {
+    Wrapper,
+    Image,
+    Content,
+    SubHeader,
+    Name,
+    Info,
+    Message
 } from "./styled";
 import { ButtonLink } from "../ButtonLink";
-import photo from "./images/Konrad_Kapica.jpg"
+import photo from "./images/Konrad_Kapica.jpg";
 import { email } from "../email";
 
 const Header = () => (
@@ -18,7 +18,7 @@ const Header = () => (
             <SubHeader>This is</SubHeader>
             <Name>Konrad Kapica</Name>
             <Info>
-            👨🏻‍💻  I'm a passionate Frontend Developer in love with React, currently looking for new job opportunities.
+                👨🏻‍💻  I'm a passionate Frontend Developer in love with React, currently looking for new job opportunities.
             </Info>
             <ButtonLink href={`mailto:${email}`}>
                 <Message />
