bbaab32d1cbbaa9acbce24c56c2ba9569bbaaa39
Konrad Kapica <konradkapica@gmail.com>
2023-02-07 20:54:51 +0100

    personalHompeage.js changed to index.js

--- src/features/PersonalHomepage/index.js
+++ src/features/PersonalHomepage/index.js
@@ -3,12 +3,14 @@ import Header from "./Header";
 import Tile from "./Tile";
 import Footer from "./Footer";
 import Portfolio from "./Portfolio";
-import { skills } from "./Tile/skills"
-import { skillsToLearn } from "./Tile/skillsToLearn"
+import { skills } from "./Tile/skills";
+import { skillsToLearn } from "./Tile/skillsToLearn";
+import { ThemeSwitch } from "../../common/ThemeSwitch/index";
 
 function PersonalHomepage() {
   return (
     <Container>
+      <ThemeSwitch />
       <Header />
       <Tile
         title="My skillset includes 🛠️"
