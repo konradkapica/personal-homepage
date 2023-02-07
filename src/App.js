b9c28b709d2f3ad4e55091d36c93016e62906ce7
Konrad Kapica <konradkapica@gmail.com>
2023-02-07 14:08:35 +0100

    styled-normalize added, App.js moved to core

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import PersonalHomepage from "./features/PersonalHomepage/PersonalHomepage";
-
-function App() {
-    return (
-        <PersonalHomepage />
-    )
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './core/App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from 'styled-components';
 import { Provider } from "react-redux";
-import { GlobalStyle } from './GlobalStyle';
-import { theme } from './theme';
-import store from "./store"
+import store from "./core/store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
         <App />
-      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
