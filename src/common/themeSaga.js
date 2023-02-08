a2e23228401f851223de1e3b0275a6d71167533b
Konrad Kapica <konradkapica@gmail.com>
2023-02-08 11:17:24 +0100

    saving theme in local storage

---
+++ src/common/themeSaga.js
@@ -0,0 +1,12 @@
+import { select, call, takeLatest } from "redux-saga/effects";
+import { toggleTheme, selectIsDarkTheme } from "./themeSlice";
+import { saveIsDarkThemeInLocalStorage } from "./isDarkThemeLocalStorage";
+
+function* saveIsDarkThemeInLocalStorageHandler() {
+    const isDarkTheme = yield select(selectIsDarkTheme);
+    yield call(saveIsDarkThemeInLocalStorage, isDarkTheme);
+}
+
+export function* themeSaga() {
+    yield takeLatest(toggleTheme.type, saveIsDarkThemeInLocalStorageHandler);
+}
--- src/core/saga.js
+++ src/core/saga.js
@@ -1,8 +1,10 @@
 import { all } from "redux-saga/effects";
+import { themeSaga } from "../common/themeSaga";
 import { personalHomepageSaga } from "../features/PersonalHomepage/personalHomepageSaga";
 
 export default function* saga() {
     yield all([
         personalHomepageSaga(),
+        themeSaga(),
     ]);
 }
